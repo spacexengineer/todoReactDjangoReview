0ee8d4e112e413d4af85e47cd87eb0833f8b5325
Hunter Casbeer <hcasbeer@bayareanewsgroup.com>
2019-08-14 21:13:04 -0700

    Add TodoView

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import TodoSerializer
+from .models import Todo
 
-# Create your views here.
+
+class TodoView(viewsets.ModelViewSet):
+    """
+    More on what viewsets are 
+    https://www.django-rest-framework.org/api-guide/viewsets/
+
+    tldr:  a type of class-based View, that does not provide any 
+    method handlers such as .get() or .post(), 
+    and instead provides actions such as .list() and .create()
+
+    The viewsets base class provides the implementation for CRUD 
+    operations by default, what we had to do was specify the serializer
+    class and the query set.
+    """
+    serializer_class = TodoSerializer
+    queryset = Todo.objects.all()
